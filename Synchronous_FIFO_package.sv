d12b37fd6629ea8bc84732e6efebc4f5a75d7553
Ashish Suri <75773071+KnowAashish@users.noreply.github.com>
2023-10-22 03:04:28 -0400

    Synchronous FIFO - Package

---
+++ Synchronous_FIFO_package.sv
@@ -0,0 +1,10 @@
+package Synchronous_FIFO_package;
+
+`include "Synchronous_FIFO_transaction.sv"
+`include "Synchronous_FIFO_generator.sv"
+`include "Synchronous_FIFO_driver.sv"
+`include "Synchronous_FIFO_monitor.sv"
+`include "Synchronous_FIFO_scoreboard.sv"
+`include "Synchronous_FIFO_env.sv"
+
+endpackage
